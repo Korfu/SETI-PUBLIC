89dbfcc9f2a5f922edc69c10aedec1489130bcdb
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-03-07 11:03:32 +0100

    Added binary_to_decimal conversion

--- seti.py
+++ seti.py
@@ -1,8 +1,17 @@
 binary = []
-def main():
+def decimal_to_binary_activted():
     user_input = int(input("Please provide a number to convert: "))
     decimal_to_binary(user_input)
     print(binary)
+
+def binary_to_decimal_activted():
+    user_input = input("Please provide a number to convert: ")
+    input_numbers = []
+    for i in user_input:
+        if i.isdigit():
+            input_numbers.append(int(i))
+    result = binary_to_decimal(input_numbers)
+    print(result)
 
 def decimal_to_binary(user_input):
     """Returns the array of digits in binary representation of a decimal number"""
@@ -12,8 +21,12 @@ def decimal_to_binary(user_input):
 
 def binary_to_decimal(binary_digits):
     """Returns the decimal (number) representation of a binary number represented by an array of 0/1 digits"""
-    pass
-
+    result = 0 
+    exponent = 0
+    for element in reversed(binary_digits):
+        result += element*pow(2, exponent)
+        exponent += 1
+    return result
 
 def decimal_to_base(decimal_number, destination_base):
     """Returns the digits in destination_base representation of the decimal number"""
@@ -34,4 +47,4 @@ def convert_base(original_digits, original_base, destination_base):
     """Conversion from any base to any other base"""
     pass
 
-main()
+binary_to_decimal_activted()

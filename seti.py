f6a7b737c348c88c00908a4c2ea601f836ebc4d3
Venebrous <goscin3@gmail.com>
2021-03-07 10:56:43 +0100

    Added binary to decimal function

--- seti.py
+++ seti.py
@@ -1,8 +1,15 @@
 binary = []
 def main():
-    user_input = int(input("Please provide a number to convert: "))
-    decimal_to_binary(user_input)
-    print(binary)
+    #user_input = int(input("Please provide a number to convert:"))
+    #decimal_to_binary(user_input)
+    #print(binary)
+    user_input = input("Please provide a number to convert: ")
+    input_numbers = []
+    for i in user_input:
+        if i.isdigit():
+            input_numbers.append(int(i))
+    result = binary_to_decimal(input_numbers)
+    print(result)
 
 def decimal_to_binary(user_input):
     """Returns the array of digits in binary representation of a decimal number"""
@@ -11,8 +18,13 @@ def decimal_to_binary(user_input):
     binary.append(user_input % 2)
 
 def binary_to_decimal(binary_digits):
-    """Returns the decimal (number) representation of a binary number represented by an array of 0/1 digits"""
-    pass
+    result = 0 
+    exponent = 0
+    for element in reversed(binary_digits):
+        result += element*pow(2, exponent)
+        exponent += 1
+    return result
+    
 
 
 def decimal_to_base(decimal_number, destination_base):

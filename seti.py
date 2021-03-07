85a8e076aaf2bd8f27d8a397be9d5474b33e1284
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-03-07 11:12:39 +0100

    Added the decimal_to_base convertion

--- seti.py
+++ seti.py
@@ -13,6 +13,12 @@ def binary_to_decimal_activted():
     result = binary_to_decimal(input_numbers)
     print(result)
 
+def decimal_to_base_activated():
+    user_input = int(input("Please provide a number "))
+    destination_base = int(input("Enter base: "))
+    decimal_to_base(user_input, destination_base)
+    print(binary)
+
 def decimal_to_binary(user_input):
     """Returns the array of digits in binary representation of a decimal number"""
     if user_input > 1:
@@ -30,7 +36,9 @@ def binary_to_decimal(binary_digits):
 
 def decimal_to_base(decimal_number, destination_base):
     """Returns the digits in destination_base representation of the decimal number"""
-    pass
+    if decimal_number > (destination_base-1):
+        decimal_to_base(decimal_number // destination_base, destination_base)
+    binary.append(decimal_number % destination_base)
 
 
 def base_to_decimal(digits, original_base):
@@ -47,4 +55,4 @@ def convert_base(original_digits, original_base, destination_base):
     """Conversion from any base to any other base"""
     pass
 
-binary_to_decimal_activted()
+decimal_to_base_activated()

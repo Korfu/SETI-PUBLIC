d1e454b9b0c76f11679caee4d1fdf440605217a6
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-03-07 12:00:51 +0100

    Added digits_as_strings function

--- seti.py
+++ seti.py
@@ -4,6 +4,13 @@ def decimal_to_binary_activted():
     decimal_to_binary(user_input)
     print(binary)
 
+def decimal_to_binary(user_input):
+    """Returns the array of digits in binary representation of a decimal number"""
+    if user_input > 1:
+        decimal_to_binary(user_input//2)
+    binary.append(user_input % 2)
+
+
 def binary_to_decimal_activted():
     user_input = input("Please provide a number to convert: ")
     input_numbers = []
@@ -13,11 +20,28 @@ def binary_to_decimal_activted():
     result = binary_to_decimal(input_numbers)
     print(result)
 
+def binary_to_decimal(binary_digits):
+    """Returns the decimal (number) representation of a binary number represented by an array of 0/1 digits"""
+    result = 0 
+    exponent = 0
+    for element in reversed(binary_digits):
+        result += element*pow(2, exponent)
+        exponent += 1
+    return result
+
+
 def decimal_to_base_activated():
     user_input = int(input("Please provide a number "))
     destination_base = int(input("Enter base: "))
     decimal_to_base(user_input, destination_base)
     print(binary)
+
+def decimal_to_base(decimal_number, destination_base):
+    """Returns the digits in destination_base representation of the decimal number"""
+    if decimal_number > (destination_base-1):
+        decimal_to_base(decimal_number // destination_base, destination_base)
+    binary.append(decimal_number % destination_base)
+
 
 def base_to_decimal_activated():
     user_input = input("Please provide a number to convert: ")
@@ -29,27 +53,6 @@ def base_to_decimal_activated():
     result = base_to_decimal(input_numbers, destination_base)
     print(result)
 
-def decimal_to_binary(user_input):
-    """Returns the array of digits in binary representation of a decimal number"""
-    if user_input > 1:
-        decimal_to_binary(user_input//2)
-    binary.append(user_input % 2)
-
-def binary_to_decimal(binary_digits):
-    """Returns the decimal (number) representation of a binary number represented by an array of 0/1 digits"""
-    result = 0 
-    exponent = 0
-    for element in reversed(binary_digits):
-        result += element*pow(2, exponent)
-        exponent += 1
-    return result
-
-def decimal_to_base(decimal_number, destination_base):
-    """Returns the digits in destination_base representation of the decimal number"""
-    if decimal_number > (destination_base-1):
-        decimal_to_base(decimal_number // destination_base, destination_base)
-    binary.append(decimal_number % destination_base)
-
 def base_to_decimal(input_numbers, destination_base):
     """Returns the decimal (number) representation of an array of digits given in original_base"""
     result = 0 
@@ -60,13 +63,27 @@ def base_to_decimal(input_numbers, destination_base):
     return result
 
 
-def digits_as_string(digits, base):
-    """Returns the string representation of an array of digits given in base"""
-    pass
+def digits_as_string_activated():
+    user_input = input("Please provide a number to convert: ")
+    destination_base = int(input("Please provide a number base: "))
+    max_base = 16
+    if (destination_base > 16):
+        raise ValueError(f"Base number cannot be greater than {max_base}")
+    input_numbers = user_input.replace("[","").replace("]","").split(",")
+    input_number_as_int = list(map(int,input_numbers))
+    result = digits_as_string(input_number_as_int)
+    print(result)
 
+def digits_as_string(input_numbers):
+    """Returns the string representation of an array of digits given in base"""
+    dictionary = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
+    results_list = []
+    for i in input_numbers:
+        results_list.append(dictionary[i])
+    return "".join(results_list)
 
 def convert_base(original_digits, original_base, destination_base):
     """Conversion from any base to any other base"""
     pass
 
-base_to_decimal_activated()
+digits_as_string_activated()
